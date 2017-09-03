1cf27a7e11d18520c4488da7ac95924e9b75d883
Koichiro Kondo <kondo@kondo.local>
2017-09-03 19:04:37 +0900

    initial commit

---
+++ SampleProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleProject
+//
+//  Created by 近藤浩市郎 on 2017/09/03.
+//  Copyright © 2017 Koichiro Kondo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
