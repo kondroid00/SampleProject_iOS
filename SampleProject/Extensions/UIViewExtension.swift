4234716a45eecd92bd44a6733a61a04133cf0200
Koichiro Kondo <kondo@kondo.local>
2017-09-03 23:47:05 +0900

    Top画面作成

---
+++ SampleProject/Extensions/UIViewExtension.swift
@@ -0,0 +1,43 @@
+//
+//  UIViewExtension.swift
+//  SampleProject
+//
+//  Created by 近藤浩市郎 on 2017/09/03.
+//  Copyright © 2017 Koichiro Kondo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    // 枠線の色
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return layer.borderColor.map { UIColor(cgColor: $0) }
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+    
+    // 枠線のWidth
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    // 角丸設定
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+}
