6d5f2aa56cd62f82daf9093258d8cc1c923050e9
Koichiro Kondo <kondo@kondo.local>
2017-10-10 20:50:12 +0900

    枠線実装

--- SampleProject/Extensions/UIViewExtension.swift
+++ SampleProject/Extensions/UIViewExtension.swift
@@ -11,6 +11,13 @@ import UIKit
 
 extension UIView {
     
+    enum BorderSide {
+        case Top
+        case Right
+        case Bottom
+        case Left
+    }
+    
     // 枠線の色
     @IBInspectable var borderColor: UIColor? {
         get {
@@ -40,4 +47,41 @@ extension UIView {
             layer.cornerRadius = newValue
         }
     }
+    
+    
+    //-------------------------------------------------------------------------
+    // コーディング
+    //-------------------------------------------------------------------------
+    // 枠線
+    func addBorder(borderSide: [BorderSide], width: CGFloat, color: UIColor) {
+        self.layer.sublayers = nil
+        self.layer.masksToBounds = true
+        
+        if borderSide.contains(.Top) {
+            let topLine = CALayer()
+            topLine.backgroundColor = color.cgColor
+            topLine.frame = CGRect(x: 0.0, y: 0.0, width: self.frame.width, height: width)
+            self.layer.addSublayer(topLine)
+        }
+        if borderSide.contains(.Left) {
+            let leftLine = CALayer()
+            leftLine.backgroundColor = color.cgColor
+            leftLine.frame = CGRect(x: 0.0, y: 0.0, width: width, height: self.frame.height)
+            self.layer.addSublayer(leftLine)
+        }
+        if borderSide.contains(.Bottom) {
+            let bottomLine = CALayer()
+            bottomLine.backgroundColor = color.cgColor
+            bottomLine.frame = CGRect(x: 0.0, y: self.frame.height - width, width: self.frame.width, height: width)
+            self.layer.addSublayer(bottomLine)
+        }
+        if borderSide.contains(.Right) {
+            let rightLine = CALayer()
+            rightLine.backgroundColor = color.cgColor
+            rightLine.frame = CGRect(x: self.frame.width - width, y: 0.0, width: width, height: self.frame.height)
+            self.layer.addSublayer(rightLine)
+        }
+    }
+
+    
 }
